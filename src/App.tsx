82f258fb123c0ea7af64946976bc6813c7831be5
awsdang <awsdang@gmail.com>
2025-01-20 16:08:14 +0300

    fetch data from API and set it to a state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import Movie from "./types/movie";
 
 
-function App() {
+const API_URL = "/api/guK8Sdo"
+
+
+const App = () => {
+  const [ data,setData ] = useState<Movie>()
+  useEffect(() => {
+    if(data) return;
+    const fetchDataAsync = async () => {
+      const response = await axios.get(API_URL);
+      if (response.status !== 200) {
+        new Error("Error fetching data");
+      }
+      setData(response.data);
+    }
+   fetchDataAsync();
+  },[data])
 
   return (
     <>
-      <div className="bg-blue-500 text-white text-center p-4">
-        hello world
+     <main className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-8">Movie Data Visualization</h1>
+      <h3 className="text-xl font-bold mb-8">Made by Aws Abdulfattah for Glance Care/Screening Test</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+    
       </div>
+    </main>
     </>
   )
 }
---
+++ src/types/movie.ts
@@ -0,0 +1,15 @@
+interface Movie {
+  title: string;
+  year: string;
+  genre: string[];
+  country: string[];
+  imdb_rating: number;
+  oscar_nominations: number;
+  oscar_winning: number;
+  cast: string[];
+  language: string[];
+  oscar_nominations_list: string[];
+  oscar_winning_list: string[];
+}
+
+export default Movie;
