c2293af1563aabdab3faae9fb44714790269bf25
Aws <awsdang@gmail.com>
2025-01-21 14:15:20 +0300

    parse movies into cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Movie from "./types/movie";
 import { SearchElement } from "./components/search";
 import { FilterElement } from "./components/filter";
 import { MovieTimeline } from "./components/timeline";
+import { MovieCard } from "./components/movieCard";
 
 
 
@@ -17,7 +18,7 @@ const removeDuplicates = (movies: Movie[]): Movie[] => {
       return false;
     } else {
       seenTitles.add(movie.title);
-      return true; 
+      return true;
     }
   });
 };
@@ -25,7 +26,7 @@ const removeDuplicates = (movies: Movie[]): Movie[] => {
 const App = () => {
   const [data, setData] = useState<Movie[]>()
   const [movies, setMovies] = useState<Movie[]>();
-  const [duplicates, setDuplicates] = useState<boolean>(false);
+
 
   useMemo(() => {
     if (data) return;
@@ -42,43 +43,44 @@ const App = () => {
 
   useEffect(() => {
     if (!data) return
-      setMovies(data)
+    setMovies(data)
   }, [data])
 
   return (
     <>
-    
-      <main className="mx-auto p-4 bg-gray-900 h-full text-white w-full">
+
+      <main className="mx-auto p-4 bg-gray-900 min-h-screen h-full text-white w-full">
         <div className="max-w-6xl mx-auto">
           <h1 className="text-4xl font-bold mb-2">Movie Data Visualization</h1>
           <h3 className="text-md mb-8">Made by {' '}
             <a className="text-blue-200" href="https://linkedin.com/in/awsdang">Aws Abdulfattah</a> {' '}for Glance Care/Screening Test
           </h3>
-          {data && movies && 
+          {data && movies &&
             <>
-          <div className="bg-gray-800 p-4 rounded-lg mb-4 shadow flex flex-row justify-between px-8 gap-4">
-            <div className="w-1/2">
-              <SearchElement data={data} movies={movies} onFilter={setMovies} />
-            </div>
-            <div className="w-1/2">
-              <FilterElement data={data} movies={movies} onFilter={setMovies} />
-            </div>
-            
-            
-          </div>
+              <div className="bg-gray-800 p-4 rounded-lg mb-4 shadow flex flex-row justify-between px-8 gap-4">
+                <div className="w-1/2">
+                  <SearchElement data={data} movies={movies} onFilter={setMovies} />
+                </div>
+                <div className="w-1/2">
+                  <FilterElement data={data} movies={movies} onFilter={setMovies} />
+                </div>
 
-<MovieTimeline movies={movies} />
-</>
-            }
-  
-            
+
+              </div>
+
+              <MovieTimeline movies={movies} />
+
+
+            </>
+          }
+
+          
+          
           <div className="">
-            <div className="bg-gray-800 p-4 rounded-lg">
-              <h2 className="text-2xl font-bold mb-2">Movie Data</h2>
-              <p className="text-md">This is the movie data fetched from the API after filters & search</p>
-              <pre className="bg-gray-900 p-4 rounded-lg overflow-auto">
-                {JSON.stringify(movies, null, 2)}
-              </pre>
+            <div className="bg-gray-800 p-2 rounded-lg grid grid-cols-1 md:grid-cols-2 gap-4">
+            {movies && movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
             </div>
 
           </div>
---
+++ src/components/movieCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Star, Award, Globe2, Trophy } from 'lucide-react';
+import Movie from '../types/movie';
+
+interface Props {
+  movie: Movie;
+}
+
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+  return (
+    <div className="bg-gray-700 p-6 rounded-lg shadow-lg">
+      <h3 className="text-xl font-bold mb-2">{movie.title}</h3>
+      <div className="flex items-center gap-2 text-gray-300 mb-2">
+        <span>{movie.year}</span>
+        <span>•</span>
+        <div className="flex items-center">
+          <Star className="w-4 h-4 text-yellow-400 mr-1" />
+          <span>{movie.imdb_rating}</span>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-2 mb-4">
+        {movie.genre.map(g => (
+          <span key={g} className="px-2 py-1 bg-gray-800 text-white rounded-lg text-sm">
+            {g}
+          </span>
+        ))}
+      </div>
+      <div className="flex items-center gap-4 mb-4">
+      <div className="flex items-center">
+          <Award className="w-5 h-5 text-gold-500 mr-2" />
+          <div>
+            <div className="text-xs text-gray-400">Oscar Nominations</div>
+            <div className="font-semibold">{movie.oscar_nominations}</div>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <Trophy className="w-5 h-5 text-gold-500 mr-2" />
+          <div>
+            <div className="text-xs text-gray-400">Oscar Wins</div>
+            <div className="font-semibold">{movie.oscar_winning}</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center mb-2">
+          <div>
+            <div className="text-xs text-gray-400">Languages</div>
+            <div className="text-sm font-semibold">{movie.language.join(', ')}</div>
+          </div>
+        </div>
+      <div className="text-sm text-gray-200">
+        <strong>Cast:</strong> {movie.cast.join(', ')}
+      </div>
+    </div>
+  );
+};
