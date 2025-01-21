aebabab925d73115a73de47480c588b83652327c
Aws <awsdang@gmail.com>
2025-01-22 00:22:18 +0300

    fix typo & move RenderActiveShape function to lib

--- src/components/countryLanguagePie.tsx
+++ src/components/countryLanguagePie.tsx
@@ -1,57 +1,29 @@
 import React, { useState } from 'react';
-import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 import Movie from '../types/movie';
-
+import { RenderActiveShape } from './renderActiveShape';
 interface Props {
     movies: Movie[];
 }
 
-const renderActiveShape = (props: any) => {
-    const RADIAN = Math.PI / 180;
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-    const sin = Math.sin(-RADIAN * midAngle);
-    const cos = Math.cos(-RADIAN * midAngle);
-    const sx = cx + (outerRadius + 10) * cos;
-    const sy = cy + (outerRadius + 10) * sin;
-    const mx = cx + (outerRadius + 30) * cos;
-    const my = cy + (outerRadius + 30) * sin;
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-    const ey = my;
-    const textAnchor = cos >= 0 ? 'start' : 'end';
 
-    return (
-        <g>
-            <text x={cx} y={cy} dy={8} textAnchor="middle" fill={'#fff'}>
-                {payload.name}
-            </text>
-            <Sector
-                cx={cx}
-                cy={cy}
-                innerRadius={innerRadius}
-                outerRadius={outerRadius}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                fill={fill}
-            />
-            <Sector
-                cx={cx}
-                cy={cy}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                innerRadius={outerRadius + 6}
-                outerRadius={outerRadius + 10}
-                fill={fill}
-            />
-            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#fff">{`Count ${value}`}</text>
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#ccc">
-                {`(Rate ${(percent * 100).toFixed(2)}%)`}
-            </text>
-        </g>
-    );
-};
-
+/**
+ * CountryLanguagePies component displays two pie charts representing the distribution of languages and countries in a list of movies.
+ *
+ * @component
+ * @param {Props} props - The props for the component.
+ * @param {Array} props.movies - An array of movie objects, each containing `language` and `country` properties.
+ *
+ * @returns {JSX.Element} The rendered CountryLanguagePies component.
+ *
+ * @example
+ * const movies = [
+ *   { language: 'English', country: 'USA' },
+ *   { language: 'Spanish', country: 'Spain' },
+ *   // more movies
+ * ];
+ * <CountryLanguagePies movies={movies} />
+ */
 export const CountryLanguagePies: React.FC<Props> = ({ movies }) => {
     const [activeIndexCountry, setActiveIndexCountry] = useState(0);
     const [activeIndexLanguage, setActiveIndexLanguage] = useState(0);
@@ -99,7 +71,7 @@ export const CountryLanguagePies: React.FC<Props> = ({ movies }) => {
                     <PieChart width={800} height={800}>
                         <Pie
                             activeIndex={activeIndexLanguage}
-                            activeShape={renderActiveShape}
+                            activeShape={RenderActiveShape}
                             data={languageData}
                             cx="50%"
                             cy="50%"
@@ -116,7 +88,7 @@ export const CountryLanguagePies: React.FC<Props> = ({ movies }) => {
                     <PieChart width={800} height={800}>
                         <Pie
                             activeIndex={activeIndexCountry}
-                            activeShape={renderActiveShape}
+                            activeShape={RenderActiveShape}
                             data={countryData}
                             cx="50%"
                             cy="50%"
